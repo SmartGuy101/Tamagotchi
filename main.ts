6f812cda2f5fd2211fe61947444903cea7e4c34f
Daniel <74032978+SmartGuy101@users.noreply.github.com>
2021-08-24 11:19:30 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,17 @@
-let MyPlayer = sprites.create(assets.image`Player`, SpriteKind.Player)
-MyPlayer.setImage(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `)
+controller.onGesture(ControllerGesture.TiltLeft, function () {
+    MyPlayer.setImage(assets.image`Player Left`)
+    MyPlayer.setVelocity(-50, 0)
+    MyPlayer.setStayInScreen(false)
+})
+controller.onGesture(ControllerGesture.TiltRight, function () {
+    MyPlayer.setImage(assets.image`Player Right`)
+    MyPlayer.setVelocity(50, 0)
+    MyPlayer.setStayInScreen(false)
+})
+controller.onGesture(ControllerGesture.Shake, function () {
+    MyPlayer.setImage(assets.image`Player Shake`)
+    MyPlayer.startEffect(effects.ashes)
+})
+let MyPlayer: Sprite = null
+MyPlayer = sprites.create(assets.image`Player`, SpriteKind.Player)
+scene.setBackgroundColor(1)
