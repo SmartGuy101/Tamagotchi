cfd589f7ebf61ee60b919d967fcf019bf3b4b677
Daniel <74032978+SmartGuy101@users.noreply.github.com>
2021-08-24 12:11:54 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,30 @@ controller.onGesture(ControllerGesture.TiltLeft, function () {
     MyPlayer.setImage(assets.image`Player Left`)
     MyPlayer.setVelocity(-50, 0)
     MyPlayer.setStayInScreen(false)
+    music.thump.play()
 })
 controller.onGesture(ControllerGesture.TiltRight, function () {
     MyPlayer.setImage(assets.image`Player Right`)
     MyPlayer.setVelocity(50, 0)
     MyPlayer.setStayInScreen(false)
+    music.thump.play()
 })
 controller.onGesture(ControllerGesture.Shake, function () {
     MyPlayer.setImage(assets.image`Player Shake`)
     MyPlayer.startEffect(effects.ashes)
+    music.setVolume(20)
+    music.bigCrash.play()
 })
 let MyPlayer: Sprite = null
 MyPlayer = sprites.create(assets.image`Player`, SpriteKind.Player)
 scene.setBackgroundColor(1)
+music.setVolume(255)
+music.powerUp.play()
+forever(function () {
+    if (MyPlayer.x > scene.screenWidth() - 20) {
+        MyPlayer.x = scene.screenWidth() - 21
+    }
+    if (MyPlayer.x < 1 - scene.screenWidth()) {
+        MyPlayer.x = scene.screenWidth() + 1
+    }
+})
